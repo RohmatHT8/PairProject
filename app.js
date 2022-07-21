3e3fe991c5e53f813fe4b55d9adc1b12825100d9
yasmirsa <mirsa.raib@gmail.com>
2022-07-21 08:03:27 +0700

    add_routes_revisi

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-const express = require('express');
-const app = express();
-const port = 3000
 
-app.set('view engine','ejs');
-app.use(express.urlencoded({extended:true}));
+const router = require('./routes/index');
+
+
 
 
 app.use('/',router);
 
-app.listen(port,()=>{
-    console.log(`App is listening on port ${port}`);
-})
