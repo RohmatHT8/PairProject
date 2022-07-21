fa750e00387ef4c49c73e9c0d1c21c2340051e61
Rohmat Hidayattullah <rohmathidayattullah97@gmail.com>
2022-07-22 04:27:11 +0700

    Commit TErakhir Saya kurang validasi di Post Sama User saja belum ada instance method

--- models/post.js
+++ models/post.js
@@ -4,11 +4,7 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+  
     static associate(models) {
       Post.belongsTo(models.Category)
       Post.belongsTo(models.User)
